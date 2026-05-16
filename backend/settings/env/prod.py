705baa844edc902e332bf25c8b70856051e1e8f9
elitekbtu <satbaldievst@mail.ru>
2026-05-16 17:11:47 +0500

    fix: update production settings to use environment variables for allowed hosts and CORS origins

--- backend/settings/env/prod.py
+++ backend/settings/env/prod.py
@@ -2,6 +2,7 @@
 
 # Third-party modules
 import dj_database_url
+from decouple import Csv, config
 
 # Project modules
 from settings.base import *  # noqa: F403
@@ -9,13 +10,11 @@ from settings.conf import POSTGRESQL_URL, REDIS_DB, REDIS_HOST, REDIS_PORT
 
 DEBUG: bool = False
 
-ALLOWED_HOSTS: list[str] = [
-    "localhost:8000",
-    "localhost",
-    "localhost:5173",
-    "127.0.0.1",
-    "127.0.0.1:8000",
-]
+ALLOWED_HOSTS: list[str] = config(
+    "ALLOWED_HOSTS",
+    cast=Csv(),
+    default="localhost,127.0.0.1,212.109.223.106",
+)
 
 DATABASES: dict = {
     "default": dj_database_url.parse(
@@ -25,12 +24,16 @@ DATABASES: dict = {
 
 STATICFILES_STORAGE: str = "whitenoise.storage.CompressedStaticFilesStorage"
 
-CORS_ALLOWED_ORIGINS: list[str] = [
-    "http://localhost",
-    "http://localhost:80",
-    "http://localhost:5173",
-    "http://localhost:8000",
-]
+CORS_ALLOWED_ORIGINS: list[str] = config(
+    "CORS_ALLOWED_ORIGINS",
+    cast=Csv(),
+    default="http://localhost,http://localhost:80,http://localhost:5173,http://localhost:8000,http://127.0.0.1:81,http://212.109.223.106:81",
+)
+CSRF_TRUSTED_ORIGINS: list[str] = config(
+    "CSRF_TRUSTED_ORIGINS",
+    cast=Csv(),
+    default="http://localhost,http://localhost:80,http://localhost:5173,http://localhost:8000,http://127.0.0.1:81,http://212.109.223.106:81",
+)
 CORS_ALLOW_CREDENTIALS: bool = True
 
 CACHES: dict = {
